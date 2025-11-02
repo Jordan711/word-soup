2523562520f14a0c7c34c5e3edc551bcbe05377f
jordan711 <jordan.feng@hotmail.com>
2025-11-02 16:12:24 -0500

    Generate sentence using the two separate lists

--- lib/screens/thank_you.dart
+++ lib/screens/thank_you.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:word_soup/providers/word_cart.dart';
+import 'package:word_soup/providers/word_cart/noun_cart.dart';
+import 'package:word_soup/providers/word_cart/verb_cart.dart';
 
+
+String _generateSentence(List<String> nouns, List<String> verbs) {
+  String sentence = "${(List.of([...nouns, ...verbs])..shuffle()).join(' ')}!!";
+  return sentence.toString();
+}
 
 class ThankYou extends ConsumerWidget {
   const ThankYou({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<String> wordCart = ref.watch(wordCartList);
-    String sentence = "${(List.of(wordCart)..shuffle()).join(' ')}!!";
+    final List<String> nounCart = ref.watch(nounCartList)..shuffle();
+    final List<String> verbCart = ref.watch(verbCartList)..shuffle();
 
+    final String sentence = _generateSentence(nounCart, verbCart);
 
     return Container(
       padding: EdgeInsets.all(32.0),
--- lib/screens/word_cart_page.dart
+++ lib/screens/word_cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:word_soup/providers/word_cart.dart';
+import 'package:word_soup/providers/word_cart/noun_cart.dart';
+import 'package:word_soup/providers/word_cart/verb_cart.dart';
 import 'package:word_soup/screens/thank_you.dart';
 
 class WordCartPage extends ConsumerWidget {
@@ -8,7 +9,8 @@ class WordCartPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final wordCart = ref.watch(wordCartList);
+    final List<String> nounCart = ref.watch(nounCartList);
+    final List<String> verbCart = ref.watch(verbCartList);
 
     return Container(
       padding: EdgeInsets.all(32.0),
@@ -16,11 +18,13 @@ class WordCartPage extends ConsumerWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text("Your Words"),
-          Text(wordCart.join(', ')),
+          Column(children: [Text("Your Nouns"), Text(nounCart.join(', '))]),
+          Column(children: [Text("Your Verbs"), Text(verbCart.join(', '))]),
           ElevatedButton(
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => Scaffold(body: ThankYou())));
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => Scaffold(body: ThankYou())),
+              );
             },
             child: Text("Checkout"),
           ),
